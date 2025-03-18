efd9a11534d7bec18c494d276134cac6c50f9e3b
Felipe Dutra <felipemd.dutra@gmail.com>
2025-03-18 19:51:45 -0300

    my first solution (not actually, just the first one im adding to github)

---
+++ easies/twoSum1.cpp
@@ -0,0 +1,22 @@
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+vector<int> twoSum(vector<int>& nums, int target) {
+    unordered_map<int, int> seen;
+
+    for (int i = 0; i < nums.size()-1; i++) {
+        int diff = target - nums[i]; // Find the number we need to sum up to the target.
+
+        // If that number is in our hash map, return the current number and the difference
+        if (seen.find(diff) != seen.end()) {
+            return vector {seen[diff], nums[i]};
+        }
+
+        seen[nums[i]] = i; // Else, just add the current number to our seen hash map
+    }
+
+    return {};
+}
+
