07f926755e132739d69803f5764bde3d2111566a
Felipe Dutra <felipemd.dutra@gmail.com>
2025-03-18 20:13:29 -0300

    fix small bug in for loop

--- easies/twoSum1.cpp
+++ easies/twoSum1.cpp
@@ -6,7 +6,7 @@ using namespace std;
 vector<int> twoSum(vector<int>& nums, int target) {
     unordered_map<int, int> seen;
 
-    for (int i = 0; i < nums.size()-1; i++) {
+    for (int i = 0; i < nums.size(); i++) {
         int diff = target - nums[i]; // Find the number we need to sum up to the target.
 
         // If that number is in our hash map, return the current number and the difference
